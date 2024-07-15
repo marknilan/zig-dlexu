bce1c6dc2ab059f9676fc56f5d61c6ca1399afbc
Mark Nilan <mark7498@outlook.com>
2024-07-15 16:18:46 +0800

    zig is hard

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,58 @@
+const capy = @import("capy");
 const std = @import("std");
+pub usingnamespace capy.cross_platform;
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+// Override the allocator used by Capy
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+pub const capy_allocator = gpa.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+fn draw(widget: *capy.Canvas, ctx: *capy.DrawContext) !void {
+    _ = widget;
+    std.log.info("drawing widget", .{});
+    ctx.setColor(0, 0, 0);
+    ctx.rectangle(120, 320, 50, 50);
+    ctx.fill();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    var layout = capy.DrawContext.TextLayout.init();
+    defer layout.deinit();
+    ctx.setColor(1, 1, 1);
+    layout.setFont(.{ .face = "Liberation Sans", .size = 12.0 });
+    layout.wrap = 50;
+    ctx.text(120, 320, layout, "Hello, World !");
+    ctx.fill();
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn scroll(widget: *capy.Canvas, dx: f32, dy: f32) !void {
+    std.log.info("Scroll by {d}, {d}", .{ dx, dy });
+    try widget.requestDraw();
+}
+
+pub fn main() !void {
+    try capy.backend.init();
+    defer _ = gpa.deinit();
+
+    var window = try capy.Window.init();
+    defer window.deinit();
+
+    var canvas = capy.canvas(.{});
+    try canvas.addDrawHandler(&draw);
+    try canvas.addScrollHandler(&scroll);
+
+    try window.set(capy.column(.{}, .{
+        capy.row(.{}, .{
+            capy.textField(.{ .text = " " }),
+            capy.textField(.{ .text = " " }),
+            capy.label(.{ .text = "Dlexu configuration page", .alignment = .Center }),
+            capy.expanded(capy.textField(.{ .text = "gemini://gemini.circumlunar.space/" })),
+            capy.button(.{ .label = "Go!" }),
+        }),
+        capy.textField(.{ .text = " " }),
+        capy.textField(.{ .text = " " }),
+
+        capy.expanded(canvas),
+    }));
+
+    window.setPreferredSize(800, 600);
+    window.show();
+    capy.runEventLoop();
 }
